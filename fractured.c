51c29dd8347fdbb2689069b543a0af924a3fb14d
sheasemco <sh140517@ucf.edu>
2024-09-09 02:35:10 +0000

    final final

--- fractured.c
+++ fractured.c
@@ -27,7 +27,6 @@ int main(int argc, char **argv) {
 // Function to calculate the distance between two points
 double calculateDistance() {
     double x1, y1, x2, y2, distance;
-
     // Getting the coordinates from user
     printf("Enter x1: ");
     scanf("%lf", &x1);
@@ -37,27 +36,21 @@ double calculateDistance() {
     scanf("%lf", &x2);
     printf("Enter y2: ");
     scanf("%lf", &y2);
-
     // Output points
     printf("Point #1 entered: x1 = %.2lf; y1 = %.2lf\n", x1, y1);
     printf("Point #2 entered: x2 = %.2lf; y2 = %.2lf\n", x2, y2);
-
     // Calculate the distance
     distance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
-
     // Output the distance
     printf("The distance between the two points is %.2lf\n", distance);
 
     return distance;
 }
-
 // Function to calculate the perimeter (
 double calculatePerimeter() {
     double distance = calculateDistance();
     double perimeter = PI * distance;
-
     printf("The perimeter of the city encompassed by your request is %.2lf\n", perimeter);
-
     // Return a difficulty rating 
     return 3.0;  
 }
@@ -67,9 +60,7 @@ double calculateArea() {
     double distance = calculateDistance();
     double radius = distance / 2;
     double area = PI * pow(radius, 2);
-
     printf("The area of the city encompassed by your request is %.2lf\n", area);
-
     // Return a difficulty rating 
     return 4.0;  
 }
@@ -77,20 +68,16 @@ double calculateArea() {
 // Function to calculate the width 
 double calculateWidth() {
     double x1, x2, width;
-
     // Getting the x-coordinates from user
     printf("Enter x1: ");
     scanf("%lf", &x1);
     printf("Enter x2: ");
     scanf("%lf", &x2);
-
     // Output points
     printf("Point #1 entered: x1 = %.2lf\n", x1);
     printf("Point #2 entered: x2 = %.2lf\n", x2);
-
     // Calculate the width
     width = fabs(x2 - x1);
-
     // Output the width
     printf("The width of the city encompassed by your request is %.2lf\n", width);
 
@@ -100,26 +87,21 @@ double calculateWidth() {
 // Function to calculate the height 
 double calculateHeight() {
     double y1, y2, height;
-
     // Getting the y-coordinates from user
     printf("Enter y1: ");
     scanf("%lf", &y1);
     printf("Enter y2: ");
     scanf("%lf", &y2);
-
     // Output points
     printf("Point #1 entered: y1 = %.2lf\n", y1);
     printf("Point #2 entered: y2 = %.2lf\n", y2);
-
     // Calculate the height
     height = fabs(y2 - y1);
-
     // Output the height
     printf("The height of the city encompassed by your request is %.2lf\n", height);
 
     return 2.5;  
 }
-
 // Optional bonus
 double askForUserInput() {
     double input;
